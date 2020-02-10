4b836c316a73569fcef942296498f8ed62447ceb
Zongyi Li <zli@wustl.edu>
2020-02-09 23:10:19 -0800

    UAI

---
+++ UAI1_full_resolution.py
@@ -0,0 +1,466 @@
+import torch
+import numpy as np
+
+import torch.nn.functional as F
+import torch.nn as nn
+
+from torch_geometric.data import Data, DataLoader
+import matplotlib.pyplot as plt
+from utilities import *
+from nn_conv import NNConv, NNConv_old
+
+from timeit import default_timer
+import scipy.io
+
+class KernelNN(torch.nn.Module):
+    def __init__(self, width, ker_width, depth, ker_in, in_width=1, out_width=1):
+        super(KernelNN, self).__init__()
+        self.depth = depth
+
+        self.fc1 = torch.nn.Linear(in_width, width)
+
+        kernel = DenseNet([ker_in, ker_width, ker_width, width**2], torch.nn.ReLU)
+        self.conv1 = NNConv_old(width, width, kernel, aggr='mean')
+
+        self.fc2 = torch.nn.Linear(width, 1)
+
+    def forward(self, data):
+        x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+        x = self.fc1(x)
+        for k in range(self.depth):
+            x = F.relu(self.conv1(x, edge_index, edge_attr))
+
+        x = self.fc2(x)
+        return x
+
+
+TRAIN_PATH = 'data/piececonst_r241_N1024_smooth1.mat'
+TEST_PATH = 'data/piececonst_r241_N1024_smooth2.mat'
+
+r = 16
+s = int(((241 - 1)/r) + 1)
+n = s**2
+m = 100
+k = 1
+
+radius_train = 0.1
+radius_test = 0.1
+
+print('resolution', s)
+
+
+ntrain = 100
+ntest = 100
+
+batch_size = 10
+batch_size2 = 2
+width = 64
+ker_width = 1000
+depth = 6
+edge_features = 6
+node_features = 6
+
+epochs = 2
+learning_rate = 0.0001
+scheduler_step = 50
+scheduler_gamma = 0.8
+
+
+path_train_err = 'results/UAI1_r'+str(s)+'_n'+ str(ntrain)+'train'
+path_test_err16 = 'results/UAI1_r'+str(s)+'_n'+ str(ntrain)+'test16'
+path_test_err31 = 'results/UAI1_r'+str(s)+'_n'+ str(ntrain)+'test31'
+path_test_err61 = 'results/UAI1_r'+str(s)+'_n'+ str(ntrain)+'test61'
+path_image_train = 'image/UAI1_r'+str(s)+'_n'+ str(ntrain)+'train'
+path_image_test16 = 'image/UAI1_r'+str(s)+'_n'+ str(ntrain)+'test16'
+path_image_test31 = 'image/UAI1_r'+str(s)+'_n'+ str(ntrain)+'test31'
+path_image_test61 = 'image/UAI1_r'+str(s)+'_n'+ str(ntrain)+'test61'
+
+t1 = default_timer()
+
+
+reader = MatReader(TRAIN_PATH)
+train_a = reader.read_field('coeff')[:ntrain,::r,::r].reshape(ntrain,-1)
+train_a_smooth = reader.read_field('Kcoeff')[:ntrain,::r,::r].reshape(ntrain,-1)
+train_a_gradx = reader.read_field('Kcoeff_x')[:ntrain,::r,::r].reshape(ntrain,-1)
+train_a_grady = reader.read_field('Kcoeff_y')[:ntrain,::r,::r].reshape(ntrain,-1)
+train_u = reader.read_field('sol')[:ntrain,::r,::r].reshape(ntrain,-1)
+train_u64 = reader.read_field('sol')[:ntrain,::r,::r].reshape(ntrain,-1)
+
+reader.load_file(TEST_PATH)
+test_a = reader.read_field('coeff')[:ntest,::4,::4].reshape(ntest,-1)
+test_a_smooth = reader.read_field('Kcoeff')[:ntest,::4,::4].reshape(ntest,-1)
+test_a_gradx = reader.read_field('Kcoeff_x')[:ntest,::4,::4].reshape(ntest,-1)
+test_a_grady = reader.read_field('Kcoeff_y')[:ntest,::4,::4].reshape(ntest,-1)
+test_u = reader.read_field('sol')[:ntest,::4,::4].reshape(ntest,-1)
+
+
+a_normalizer = GaussianNormalizer(train_a)
+train_a = a_normalizer.encode(train_a)
+test_a = a_normalizer.encode(test_a)
+as_normalizer = GaussianNormalizer(train_a_smooth)
+train_a_smooth = as_normalizer.encode(train_a_smooth)
+test_a_smooth = as_normalizer.encode(test_a_smooth)
+agx_normalizer = GaussianNormalizer(train_a_gradx)
+train_a_gradx = agx_normalizer.encode(train_a_gradx)
+test_a_gradx = agx_normalizer.encode(test_a_gradx)
+agy_normalizer = GaussianNormalizer(train_a_grady)
+train_a_grady = agy_normalizer.encode(train_a_grady)
+test_a_grady = agy_normalizer.encode(test_a_grady)
+
+
+test_a = test_a.reshape(ntest,61,61)
+test_a_smooth = test_a_smooth.reshape(ntest,61,61)
+test_a_gradx = test_a_gradx.reshape(ntest,61,61)
+test_a_grady = test_a_grady.reshape(ntest,61,61)
+test_u = test_u.reshape(ntest,61,61)
+
+test_a16 =test_a[:ntest,::4,::4].reshape(ntest,-1)
+test_a_smooth16 = test_a_smooth[:ntest,::4,::4].reshape(ntest,-1)
+test_a_gradx16 = test_a_gradx[:ntest,::4,::4].reshape(ntest,-1)
+test_a_grady16 = test_a_grady[:ntest,::4,::4].reshape(ntest,-1)
+test_u16 = test_u[:ntest,::4,::4].reshape(ntest,-1)
+test_a31 =test_a[:ntest,::2,::2].reshape(ntest,-1)
+test_a_smooth31 = test_a_smooth[:ntest,::2,::2].reshape(ntest,-1)
+test_a_gradx31 = test_a_gradx[:ntest,::2,::2].reshape(ntest,-1)
+test_a_grady31 = test_a_grady[:ntest,::2,::2].reshape(ntest,-1)
+test_u31 = test_u[:ntest,::2,::2].reshape(ntest,-1)
+test_a =test_a.reshape(ntest,-1)
+test_a_smooth = test_a_smooth.reshape(ntest,-1)
+test_a_gradx = test_a_gradx.reshape(ntest,-1)
+test_a_grady = test_a_grady.reshape(ntest,-1)
+test_u = test_u.reshape(ntest,-1)
+
+
+u_normalizer = GaussianNormalizer(train_u)
+train_u = u_normalizer.encode(train_u)
+# test_u = y_normalizer.encode(test_u)
+
+
+
+meshgenerator = SquareMeshGenerator([[0,1],[0,1]],[s,s])
+edge_index = meshgenerator.ball_connectivity(radius_train)
+grid = meshgenerator.get_grid()
+# meshgenerator.get_boundary()
+# edge_index_boundary = meshgenerator.boundary_connectivity2d(stride = stride)
+
+data_train = []
+for j in range(ntrain):
+    edge_attr = meshgenerator.attributes(theta=train_a[j,:])
+    # edge_attr_boundary = meshgenerator.attributes_boundary(theta=train_u[j,:])
+    data_train.append(Data(x=torch.cat([grid, train_a[j,:].reshape(-1, 1),
+                                        train_a_smooth[j,:].reshape(-1, 1), train_a_gradx[j,:].reshape(-1, 1), train_a_grady[j,:].reshape(-1, 1)
+                                        ], dim=1),
+                           y=train_u[j,:], coeff=train_a[j,:],
+                           edge_index=edge_index, edge_attr=edge_attr,
+                           # edge_index_boundary=edge_index_boundary, edge_attr_boundary= edge_attr_boundary
+                           ))
+
+print('train grid', grid.shape, 'edge_index', edge_index.shape, 'edge_attr', edge_attr.shape)
+
+meshgenerator = SquareMeshGenerator([[0,1],[0,1]],[16,16])
+edge_index = meshgenerator.ball_connectivity(radius_test)
+grid = meshgenerator.get_grid()
+# meshgenerator.get_boundary()
+# edge_index_boundary = meshgenerator.boundary_connectivity2d(stride = stride)
+data_test16 = []
+for j in range(ntest):
+    edge_attr = meshgenerator.attributes(theta=test_a16[j,:])
+    # edge_attr_boundary = meshgenerator.attributes_boundary(theta=test_a[j, :])
+    data_test16.append(Data(x=torch.cat([grid, test_a16[j,:].reshape(-1, 1),
+                                       test_a_smooth16[j,:].reshape(-1, 1), test_a_gradx16[j,:].reshape(-1, 1), test_a_grady16[j,:].reshape(-1, 1)
+                                       ], dim=1),
+                           y=test_u16[j, :], coeff=test_a16[j,:],
+                           edge_index=edge_index, edge_attr=edge_attr,
+                           # edge_index_boundary=edge_index_boundary, edge_attr_boundary=edge_attr_boundary
+                          ))
+
+print('16 grid', grid.shape, 'edge_index', edge_index.shape, 'edge_attr', edge_attr.shape)
+# print('edge_index_boundary', edge_index_boundary.shape, 'edge_attr', edge_attr_boundary.shape)
+
+meshgenerator = SquareMeshGenerator([[0,1],[0,1]],[31,31])
+edge_index = meshgenerator.ball_connectivity(radius_test)
+grid = meshgenerator.get_grid()
+# meshgenerator.get_boundary()
+# edge_index_boundary = meshgenerator.boundary_connectivity2d(stride = stride)
+data_test31 = []
+for j in range(ntest):
+    edge_attr = meshgenerator.attributes(theta=test_a31[j,:])
+    # edge_attr_boundary = meshgenerator.attributes_boundary(theta=test_a[j, :])
+    data_test31.append(Data(x=torch.cat([grid, test_a31[j,:].reshape(-1, 1),
+                                       test_a_smooth31[j,:].reshape(-1, 1), test_a_gradx31[j,:].reshape(-1, 1), test_a_grady31[j,:].reshape(-1, 1)
+                                       ], dim=1),
+                           y=test_u31[j, :], coeff=test_a31[j,:],
+                           edge_index=edge_index, edge_attr=edge_attr,
+                           # edge_index_boundary=edge_index_boundary, edge_attr_boundary=edge_attr_boundary
+                          ))
+
+print('31 grid', grid.shape, 'edge_index', edge_index.shape, 'edge_attr', edge_attr.shape)
+# print('edge_index_boundary', edge_index_boundary.shape, 'edge_attr', edge_attr_boundary.shape)
+
+meshgenerator = SquareMeshGenerator([[0,1],[0,1]],[61,61])
+edge_index = meshgenerator.ball_connectivity(radius_test)
+grid = meshgenerator.get_grid()
+# meshgenerator.get_boundary()
+# edge_index_boundary = meshgenerator.boundary_connectivity2d(stride = stride)
+data_test61 = []
+for j in range(ntest):
+    edge_attr = meshgenerator.attributes(theta=test_a[j,:])
+    # edge_attr_boundary = meshgenerator.attributes_boundary(theta=test_a[j, :])
+    data_test61.append(Data(x=torch.cat([grid, test_a[j,:].reshape(-1, 1),
+                                       test_a_smooth[j,:].reshape(-1, 1), test_a_gradx[j,:].reshape(-1, 1), test_a_grady[j,:].reshape(-1, 1)
+                                       ], dim=1),
+                           y=test_u[j, :], coeff=test_a[j,:],
+                           edge_index=edge_index, edge_attr=edge_attr,
+                           # edge_index_boundary=edge_index_boundary, edge_attr_boundary=edge_attr_boundary
+                          ))
+
+print('61 grid', grid.shape, 'edge_index', edge_index.shape, 'edge_attr', edge_attr.shape)
+# print('edge_index_boundary', edge_index_boundary.shape, 'edge_attr', edge_attr_boundary.shape)
+
+train_loader = DataLoader(data_train, batch_size=batch_size, shuffle=True)
+test_loader16 = DataLoader(data_test16, batch_size=batch_size2, shuffle=False)
+test_loader31 = DataLoader(data_test31, batch_size=batch_size2, shuffle=False)
+test_loader61 = DataLoader(data_test61, batch_size=batch_size2, shuffle=False)
+
+
+
+##################################################################################################
+
+### training
+
+##################################################################################################
+t2 = default_timer()
+
+print('preprocessing finished, time used:', t2-t1)
+device = torch.device('cuda')
+
+model = KernelNN(width,ker_width,depth,edge_features,node_features).cuda()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=5e-4)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+
+myloss = LpLoss(size_average=False)
+u_normalizer.cuda()
+
+model.train()
+ttrain = np.zeros((epochs, ))
+ttest16 = np.zeros((epochs,))
+ttest31 = np.zeros((epochs,))
+ttest61 = np.zeros((epochs,))
+
+for ep in range(epochs):
+    t1 = default_timer()
+    train_mse = 0.0
+    train_l2 = 0.0
+    for batch in train_loader:
+        batch = batch.to(device)
+
+        optimizer.zero_grad()
+        out = model(batch)
+        mse = F.mse_loss(out.view(-1, 1), batch.y.view(-1,1))
+        # mse.backward()
+        loss = torch.norm(out.view(-1) - batch.y.view(-1),1)
+        loss.backward()
+
+        l2 = myloss(out.view(batch_size,-1), batch.y.view(batch_size, -1))
+        # l2.backward()
+
+        optimizer.step()
+        train_mse += mse.item()
+        train_l2 += l2.item()
+
+    scheduler.step()
+    t2 = default_timer()
+
+    model.eval()
+    test_l2_16 = 0.0
+    test_l2_31 = 0.0
+    test_l2_61 = 0.0
+
+    ttrain[ep] = train_l2/(ntrain * k)
+    ttest16[ep] = test_l2_16 / ntest
+    ttest31[ep] = test_l2_31 / ntest
+    ttest61[ep] = test_l2_61 / ntest
+
+    print(ep, ' time:', t2-t1, ' train_mse:', train_mse/len(train_loader))
+
+t1 = default_timer()
+u_normalizer.cpu()
+model = model.cpu()
+with torch.no_grad():
+    for batch in test_loader16:
+        out = model(batch)
+        test_l2_16 += myloss(u_normalizer.decode(out.view(batch_size2,-1)), batch.y.view(batch_size2, -1))
+    for batch in test_loader31:
+        out = model(batch)
+        test_l2_31 += myloss(u_normalizer.decode(out.view(batch_size2, -1)),
+                             batch.y.view(batch_size2, -1))
+    for batch in test_loader61:
+        out = model(batch)
+        test_l2_61 += myloss(u_normalizer.decode(out.view(batch_size2, -1)),
+                             batch.y.view(batch_size2, -1))
+
+ttest16[ep] = test_l2_16 / ntest
+ttest31[ep] = test_l2_31 / ntest
+ttest61[ep] = test_l2_61 / ntest
+t2 = default_timer()
+
+print(' time:', t2-t1, ' train_mse:', train_mse/len(train_loader),
+          ' test16:', test_l2_16/ntest,  ' test31:', test_l2_31/ntest,  ' test61:', test_l2_61/ntest)
+np.savetxt(path_train_err + '.txt', ttrain)
+np.savetxt(path_test_err16 + '.txt', ttest16)
+np.savetxt(path_test_err31 + '.txt', ttest31)
+np.savetxt(path_test_err61 + '.txt', ttest61)
+
+
+##################################################################################################
+
+### Ploting
+
+##################################################################################################
+
+
+plt.figure()
+# plt.plot(ttrain, label='train loss')
+plt.plot(ttest16, label='test16 loss')
+plt.plot(ttest31, label='test31 loss')
+plt.plot(ttest61, label='test61 loss')
+plt.legend(loc='upper right')
+plt.show()
+
+
+resolution = s
+data = train_loader.dataset[0].to(device)
+coeff = data.coeff.detach().cpu().numpy().reshape((resolution, resolution))
+truth = data.y.detach().cpu().numpy().reshape((resolution, resolution))
+approx = model(data).detach().cpu().numpy().reshape((resolution, resolution))
+_min = np.min(np.min(truth))
+_max = np.max(np.max(truth))
+
+plt.figure()
+plt.subplot(1, 3, 1)
+plt.imshow(truth, vmin = _min, vmax=_max)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Ground Truth')
+
+plt.subplot(1, 3, 2)
+plt.imshow(approx, vmin = _min, vmax=_max)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Approximation')
+
+plt.subplot(1, 3, 3)
+plt.imshow((approx - truth) ** 2)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Error')
+
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.savefig(path_image_train + '.png')
+
+
+resolution = 16
+data = test_loader16.dataset[0].to(device)
+coeff = data.coeff.detach().cpu().numpy().reshape((resolution, resolution))
+truth = data.y.detach().cpu().numpy().reshape((resolution, resolution))
+approx = model(data).detach().cpu().numpy().reshape((resolution, resolution))
+_min = np.min(np.min(truth))
+_max = np.max(np.max(truth))
+
+plt.figure()
+plt.subplot(1, 3, 1)
+plt.imshow(truth, vmin = _min, vmax=_max)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Ground Truth')
+
+plt.subplot(1, 3, 2)
+plt.imshow(approx, vmin = _min, vmax=_max)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Approximation')
+
+plt.subplot(1, 3, 3)
+plt.imshow((approx - truth) ** 2)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Error')
+
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.savefig(path_image_test16 + '.png')
+
+resolution = 31
+data = test_loader31.dataset[0].to(device)
+coeff = data.coeff.detach().cpu().numpy().reshape((resolution, resolution))
+truth = data.y.detach().cpu().numpy().reshape((resolution, resolution))
+approx = u_normalizer.decode(model(data).reshape(1,-1)).detach().cpu().numpy().reshape((resolution, resolution))
+_min = np.min(np.min(truth))
+_max = np.max(np.max(truth))
+
+# plt.figure()
+plt.figure()
+plt.subplot(1, 3, 1)
+plt.imshow(truth, vmin = _min, vmax=_max)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Ground Truth')
+
+plt.subplot(1, 3, 2)
+plt.imshow(approx, vmin = _min, vmax=_max)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Approximation')
+
+plt.subplot(1, 3, 3)
+plt.imshow((approx - truth) ** 2)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Error')
+
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.savefig(path_image_test31 + '.png')
+
+
+resolution = 61
+data = test_loader61.dataset[0].to(device)
+coeff = data.coeff.detach().cpu().numpy().reshape((resolution, resolution))
+truth = data.y.detach().cpu().numpy().reshape((resolution, resolution))
+approx = u_normalizer.decode(model(data).reshape(1,-1)).detach().cpu().numpy().reshape((resolution, resolution))
+_min = np.min(np.min(truth))
+_max = np.max(np.max(truth))
+
+# plt.figure()
+plt.figure()
+plt.subplot(1, 3, 1)
+plt.imshow(truth, vmin = _min, vmax=_max)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Ground Truth')
+
+plt.subplot(1, 3, 2)
+plt.imshow(approx, vmin = _min, vmax=_max)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Approximation')
+
+plt.subplot(1, 3, 3)
+plt.imshow((approx - truth) ** 2)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.title('Error')
+
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.savefig(path_image_test61 + '.png')
---
+++ UAI4_equation_sample.py
@@ -0,0 +1,252 @@
+import torch
+import numpy as np
+
+import torch.nn.functional as F
+import torch.nn as nn
+
+from torch_geometric.data import Data, DataLoader
+import matplotlib.pyplot as plt
+from utilities import *
+from nn_conv import NNConv, NNConv_old
+
+from timeit import default_timer
+import scipy.io
+
+class KernelNN(torch.nn.Module):
+    def __init__(self, width,  depth, ker_in, in_width=1, out_width=1):
+        super(KernelNN, self).__init__()
+        self.depth = depth
+
+        self.fc1 = torch.nn.Linear(in_width, width)
+
+        kernel = DenseNet([ker_in, width//4, width], torch.nn.ReLU)
+        self.conv1 = NNConv(width, width, kernel, aggr='mean')
+
+        self.fc2 = torch.nn.Linear(width, 1)
+
+    def forward(self, data):
+        x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+        x = self.fc1(x)
+        for k in range(self.depth):
+            x = F.relu(self.conv1(x, edge_index, edge_attr))
+
+        x = self.fc2(x)
+        return x
+
+class KernelNN2(torch.nn.Module):
+    def __init__(self, width, depth, ker_in, in_width=1, out_width=1):
+        super(KernelNN2, self).__init__()
+        self.depth = depth
+
+        self.fc1 = torch.nn.Linear(in_width, width)
+
+        kernel = DenseNet([ker_in, width//4, width], torch.nn.ReLU)
+        self.conv1 = NNConv(width, width, kernel, aggr='mean')
+
+        kernel2 = DenseNet([ker_in, width * in_width], torch.nn.ReLU)
+        self.conv2 = NNConv_old(in_width, width, kernel2, aggr='mean')
+
+        self.fc2 = torch.nn.Linear(width, 1)
+
+    def forward(self, data):
+        x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+        x = self.fc1(x)
+        for k in range(self.depth):
+            x = F.relu(self.conv1(x, edge_index, edge_attr))
+
+        x = self.fc2(x)
+        return x
+
+class KernelNN3(torch.nn.Module):
+    def __init__(self, width_node, width_kernel, depth, ker_in, in_width=1, out_width=1):
+        super(KernelNN3, self).__init__()
+        self.depth = depth
+
+        self.fc1 = torch.nn.Linear(in_width, width_node)
+
+        kernel = DenseNet([ker_in, width_kernel // 2, width_kernel, width_node**2], torch.nn.ReLU)
+        self.conv1 = NNConv_old(width_node, width_node, kernel, aggr='mean')
+
+        self.fc2 = torch.nn.Linear(width_node, 1)
+
+    def forward(self, data):
+        x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+        x = self.fc1(x)
+        for k in range(self.depth):
+            x = F.relu(self.conv1(x, edge_index, edge_attr))
+
+        x = self.fc2(x)
+        return x
+
+
+TRAIN_PATH = 'data/piececonst_r241_N1024_smooth1.mat'
+TEST_PATH = 'data/piececonst_r241_N1024_smooth2.mat'
+
+for k in (2,4,8):
+    for ntrain in (10,100,1000):
+        r = 2
+        s = int(((241 - 1)/r) + 1)
+        n = s**2
+        m = 200
+        # k = 5
+
+        radius_train = 0.15
+        radius_test = 0.15
+        print('resolution', s)
+
+
+        # ntrain = 100
+        ntest = 100
+
+        if ntrain <= 50:
+            batch_size = 5
+            batch_size2 = 5
+        else:
+            batch_size = 20
+            batch_size2 = 20
+        width = 64
+        ker_width = 1000
+        depth = 6
+        edge_features = 6
+        node_features = 6
+
+        epochs = 200
+        learning_rate = 0.0001
+        scheduler_step = 50
+        scheduler_gamma = 0.5
+
+
+        path_train_err = 'results/UAI4_s'+str(s)+'_n'+ str(ntrain)+'_k'+ str(k) + 'train.txt'
+        path_test_err = 'results/UAI4_s'+str(s)+'_n'+ str(ntrain)+'_k'+ str(k) + 'test.txt'
+        path_image = 'results/UAI_s'+str(s)+'_n'+ str(ntrain)+'_k'+ str(k) + '_'
+
+
+        t1 = default_timer()
+
+
+        reader = MatReader(TRAIN_PATH)
+        train_a = reader.read_field('coeff')[:ntrain,::r,::r].reshape(ntrain,-1)
+        train_a_smooth = reader.read_field('Kcoeff')[:ntrain,::r,::r].reshape(ntrain,-1)
+        train_a_gradx = reader.read_field('Kcoeff_x')[:ntrain,::r,::r].reshape(ntrain,-1)
+        train_a_grady = reader.read_field('Kcoeff_y')[:ntrain,::r,::r].reshape(ntrain,-1)
+        train_u = reader.read_field('sol')[:ntrain,::r,::r].reshape(ntrain,-1)
+
+        reader.load_file(TEST_PATH)
+        test_a = reader.read_field('coeff')[:ntest,::r,::r].reshape(ntest,-1)
+        test_a_smooth = reader.read_field('Kcoeff')[:ntest,::r,::r].reshape(ntest,-1)
+        test_a_gradx = reader.read_field('Kcoeff_x')[:ntest,::r,::r].reshape(ntest,-1)
+        test_a_grady = reader.read_field('Kcoeff_y')[:ntest,::r,::r].reshape(ntest,-1)
+        test_u = reader.read_field('sol')[:ntest,::r,::r].reshape(ntest,-1)
+
+
+        a_normalizer = GaussianNormalizer(train_a)
+        train_a = a_normalizer.encode(train_a)
+        test_a = a_normalizer.encode(test_a)
+        as_normalizer = GaussianNormalizer(train_a_smooth)
+        train_a_smooth = as_normalizer.encode(train_a_smooth)
+        test_a_smooth = as_normalizer.encode(test_a_smooth)
+        agx_normalizer = GaussianNormalizer(train_a_gradx)
+        train_a_gradx = agx_normalizer.encode(train_a_gradx)
+        test_a_gradx = agx_normalizer.encode(test_a_gradx)
+        agy_normalizer = GaussianNormalizer(train_a_grady)
+        train_a_grady = agy_normalizer.encode(train_a_grady)
+        test_a_grady = agy_normalizer.encode(test_a_grady)
+
+        u_normalizer = UnitGaussianNormalizer(train_u)
+        train_u = u_normalizer.encode(train_u)
+        # test_u = y_normalizer.encode(test_u)
+
+
+
+        meshgenerator = RandomMeshGenerator([[0,1],[0,1]],[s,s], sample_size=m)
+        data_train = []
+        for j in range(ntrain):
+            for i in range(k):
+                idx = meshgenerator.sample()
+                grid = meshgenerator.get_grid()
+                edge_index = meshgenerator.ball_connectivity(radius_train)
+                edge_attr = meshgenerator.attributes(theta=train_a[j,:])
+                #data_train.append(Data(x=init_point.clone().view(-1,1), y=train_y[j,:], edge_index=edge_index, edge_attr=edge_attr))
+                data_train.append(Data(x=torch.cat([grid, train_a[j, idx].reshape(-1, 1),
+                                                    train_a_smooth[j, idx].reshape(-1, 1), train_a_gradx[j, idx].reshape(-1, 1),
+                                                    train_a_grady[j, idx].reshape(-1, 1)
+                                                    ], dim=1),
+                                       y=train_u[j, idx], edge_index=edge_index, edge_attr=edge_attr, sample_idx=idx
+                                       ))
+
+
+        meshgenerator = RandomMeshGenerator([[0,1],[0,1]],[s,s], sample_size=m)
+        data_test = []
+        for j in range(ntest):
+            idx = meshgenerator.sample()
+            grid = meshgenerator.get_grid()
+            edge_index = meshgenerator.ball_connectivity(radius_test)
+            edge_attr = meshgenerator.attributes(theta=test_a[j,:])
+            data_test.append(Data(x=torch.cat([grid, test_a[j, idx].reshape(-1, 1),
+                                               test_a_smooth[j, idx].reshape(-1, 1), test_a_gradx[j, idx].reshape(-1, 1),
+                                               test_a_grady[j, idx].reshape(-1, 1)
+                                               ], dim=1),
+                                  y=test_u[j, idx], edge_index=edge_index, edge_attr=edge_attr, sample_idx=idx
+                                  ))
+        #
+        train_loader = DataLoader(data_train, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(data_test, batch_size=batch_size2, shuffle=False)
+
+        t2 = default_timer()
+
+        print('preprocessing finished, time used:', t2-t1)
+        device = torch.device('cuda')
+
+        model = KernelNN3(width, ker_width,depth,edge_features,in_width=node_features).cuda()
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=5e-4)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+
+        myloss = LpLoss(size_average=False)
+        u_normalizer.cuda()
+        ttrain = np.zeros((epochs, ))
+        ttest = np.zeros((epochs,))
+        model.train()
+        for ep in range(epochs):
+            t1 = default_timer()
+            train_mse = 0.0
+            train_l2 = 0.0
+            for batch in train_loader:
+                batch = batch.to(device)
+
+                optimizer.zero_grad()
+                out = model(batch)
+                mse = F.mse_loss(out.view(-1, 1), batch.y.view(-1,1))
+                mse.backward()
+
+                l2 = myloss(out.view(batch_size,-1), batch.y.view(batch_size, -1))
+
+                optimizer.step()
+                train_mse += mse.item()
+                train_l2 += l2.item()
+
+            scheduler.step()
+            t2 = default_timer()
+
+            model.eval()
+            test_l2 = 0.0
+            with torch.no_grad():
+                for batch in test_loader:
+                    batch = batch.to(device)
+                    out = model(batch)
+                    out = u_normalizer.decode(out.view(batch_size2,-1), sample_idx=batch.sample_idx.view(batch_size2,-1))
+                    test_l2 += myloss(out, batch.y.view(batch_size2, -1)).item()
+                    # test_l2 += myloss(out.view(batch_size2,-1), y_normalizer.encode(batch.y.view(batch_size2, -1))).item()
+
+            ttrain[ep] = train_mse/len(train_loader)
+            ttest[ep] = test_l2/ntest
+
+            print(k, ntrain, ep, t2-t1, train_mse/len(train_loader), train_l2/(ntrain * k), test_l2/ntest)
+
+        np.savetxt(path_train_err, ttrain)
+        np.savetxt(path_test_err, ttest)
+
+        plt.figure()
+        # plt.plot(ttrain, label='train loss')
+        plt.plot(ttest, label='test loss')
+        plt.legend(loc='upper right')
+        plt.show()
